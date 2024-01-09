9e0d329edfc717f392eb681144d1f0b7628f5036
zhangziqian <84625273+Musicminion@users.noreply.github.com>
2024-01-09 16:13:32 +0000

    Update Dockerfile paths and remove unnecessary commands

--- init.sh
+++ init.sh
@@ -2,20 +2,7 @@
 
 docker-compose -f docker-compose.image.yml pull
 
-# 把每个形如 ghcr.io/ayaka-notes/overleaf/texlive:2023
-# 的名字改成 ghcr.io/ayaka-notes/overleaf/texlive:2023.1
+docker-compose up -d
 
-# 1. 获取所有的镜像名字
-docker images --format "{{.Repository}}:{{.Tag}}" > images.txt
 
-# 2. 用正则表达式匹配，如果匹配到了，就把名字改成 ghcr.io/ayaka-notes/overleaf/texlive:2023.1
-grep -E "ghcr.io/ayaka-notes/overleaf/texlive:[0-9]{4}" images.txt | while read line; do
-    echo $line
-    new_name=$(echo $line | sed -E "s/ghcr.io\/ayaka-notes\/overleaf\/texlive:([0-9]{4})/ghcr.io\/ayaka-notes\/overleaf\/texlive:\1.1/")
-    echo $new_name
-    docker tag $line $new_name
-done
-
-# 3. 删除images.txt
-rm images.txt
-
+docker exec -it mongo mongo --eval "rs.initiate({ _id: \"overleaf\", members: [ { _id: 0, host: \"mongo:27017\" } ] })" > /dev/null
