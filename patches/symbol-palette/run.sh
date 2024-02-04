48d36bd496a964f28bb7703080a3125248e3be88
zhangziqian <84625273+Musicminion@users.noreply.github.com>
2024-02-04 14:16:22 +0000

    Add symbol palette feature and update patch files

--- patches/symbol-palette/run.sh
+++ patches/symbol-palette/run.sh
@@ -1,8 +1,15 @@
 # /bin/bash
 
+mv ./front-js-feature/symbol-palette ./services/web/frontend/js/features/
+mv ./modules/symbol-palette ./services/web/modules/
+
+
 # 插入到 overleaf/services/web/frontend/js/ide/editor/EditorManager.js 最前面
 # import '../../features/symbol-palette/controllers/symbol-palette-controller'
+sed -i "1i import '../../features/symbol-palette/controllers/symbol-palette-controller'" ./services/web/frontend/js/ide/editor/EditorManager.js
 
 # overleaf/services/web/config/settings.defaults.js
 # moduleImportSequence: 
 # 'symbol-palette' 加上这个模块
+sed -i "/moduleImportSequence: \[/a \\\
+'symbol-palette'," ./services/web/config/settings.defaults.js
