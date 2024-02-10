a97164db3bead170c92fdc9a559f5bff6d97578b
zhangziqian <84625273+Musicminion@users.noreply.github.com>
2024-02-10 09:48:54 +0000

    Roll back to no reference version

--- patch.sh
+++ patch.sh
@@ -1,22 +1,33 @@
 # /bin/bash
+
 cp -r ./patches/config-clsi-match/* ./overleaf
 cd ./overleaf && ./run.sh && cd ..
+
 cp -r ./patches/config-web-setting/* ./overleaf
 cd ./overleaf && ./run.sh && cd ..
+
 cp -r ./patches/cron-setting/* ./overleaf
 cd ./overleaf && ./run.sh && cd ..
+
 cp -r ./patches/unlock-50M-limit/* ./overleaf
 cd ./overleaf && ./run.sh && cd ..
+
 cp -r ./patches/nginx-learn-config/* ./overleaf
 cd ./overleaf && ./run.sh && cd ..
+
 cp -r ./patches/short-key-title/* ./overleaf
 cd ./overleaf && ./run.sh && cd ..
+
 cp -r ./patches/symbol-palette/* ./overleaf
 cd ./overleaf && ./run.sh && cd ..
+
 cp -r ./patches/track-change/* ./overleaf
 cd ./overleaf && ./run.sh && cd ..
-cp -r ./patches/references/* ./overleaf
-cd ./overleaf && ./run.sh && cd ..
+
+# cp -r ./patches/references/* ./overleaf
+# cd ./overleaf && ./run.sh && cd ..
+
 cp ./patches/more-theme/* ./overleaf
 cd ./overleaf && ./run.sh && rm -rf ./run.sh && cd ..
-cd ./overleaf && npm install --package-lock-only --ignore-scripts
+
+# cd ./overleaf && npm install --package-lock-only --ignore-scripts
