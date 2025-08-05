84dba0450e404e6126fa650962ea21326d289093
zhangziqian <zhangziqian@sjtu.edu.cn>
2025-08-05 08:50:35 +0000

    fix: mongo to mongo sh changes

--- aliyun-internal/init.sh
+++ aliyun-internal/init.sh
@@ -10,14 +10,14 @@ docker compose -f docker-compose.yml pull
 docker compose up -d mongo
 
 docker compose exec -T mongo sh -c '
-    while ! mongo --eval "db.version()" > /dev/null; do
+    while ! mongosh --eval "db.version()" > /dev/null; do
       echo "Waiting for Mongo..."
       sleep 1
     done
-    mongo --eval "rs.initiate({ _id: \"overleaf\", members: [ { _id: 0, host: \"mongo:27017\" } ] })" > /dev/null'
+    mongosh --eval "rs.initiate({ _id: \"overleaf\", members: [ { _id: 0, host: \"mongo:27017\" } ] })" > /dev/null'
 
 # 或者手动执行下面的命令
-# docker exec -it mongo mongo --eval "rs.initiate({ _id: \"overleaf\", members: [ { _id: 0, host: \"mongo:27017\" } ] })" > /dev/null
+# docker exec -it mongo mongosh --eval "rs.initiate({ _id: \"overleaf\", members: [ { _id: 0, host: \"mongo:27017\" } ] })" > /dev/null
 
 docker compose up -d minio
 
